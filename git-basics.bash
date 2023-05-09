06d1ce00cb4082e2c5d6428c2143c63233ea40da
Adam Hott <adamhott@hotmail.com>
2023-05-09 17:17:40 -0500

    Adding git-basics.bash

---
+++ git-basics.bash
@@ -0,0 +1,24 @@
+#Initialize
+cd myproject/
+git init
+
+#Connect to a remote r
+git remote add origin https://github.com/username/repo.git
+
+#Add file
+git add myfile.txt
+
+#Add 
+git add mydirectory/
+
+#Commit
+git commit -m "Add myfile.txt"
+
+#Push
+git push origin master
+
+#Pull
+git pull origin master
+
+#S
+git status
